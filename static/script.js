2970c80c6f0a7de5eec290b81c7eadca9e0cac89
✮✿ Nayra* ✿✮ <nayracutie7@proton.me>
2025-10-04 23:31:44 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -9,6 +9,9 @@ document.getElementById('gimmicks-pge').classList.add('hide');
 
 document.getElementById('oc-btn').classList.remove('active');
 document.getElementById('oc-pge').classList.add('hide');
+
+document.getElementById('comforts-btn').classList.remove('active');
+document.getElementById('comforts-pge').classList.add('hide');
 });
 
 /* */
@@ -22,6 +25,9 @@ document.getElementById('gimmicks-pge').classList.remove('hide');
 
 document.getElementById('oc-btn').classList.remove('active');
 document.getElementById('oc-pge').classList.add('hide');
+
+document.getElementById('comforts-btn').classList.remove('active');
+document.getElementById('comforts-pge').classList.add('hide');
 });
 
 /* */
@@ -33,11 +39,27 @@ document.getElementById('aboutme-pge').classList.add('hide');
 document.getElementById('gimmicks-btn').classList.remove('active');
 document.getElementById('gimmicks-pge').classList.add('hide');
 
-document.getElementById('comforts-btn').classList.remove('active');
-document.getElementById('comforts-pge').classList.add('hide');
-
 document.getElementById('oc-btn').classList.add('active');
 document.getElementById('oc-pge').classList.remove('hide');
+
+document.getElementById('comforts-btn').classList.remove('active');
+document.getElementById('comforts-pge').classList.add('hide');
+});
+
+/* */
+
+document.getElementById('comforts-btn').addEventListener('click', function() {
+document.getElementById('aboutme-btn').classList.remove('active');
+document.getElementById('aboutme-pge').classList.add('hide');
+
+document.getElementById('gimmicks-btn').classList.remove('active');
+document.getElementById('gimmicks-pge').classList.add('hide');
+
+document.getElementById('oc-btn').classList.remove('active');
+document.getElementById('oc-pge').classList.add('hide');
+
+document.getElementById('comforts-btn').classList.add('active');
+document.getElementById('comforts-pge').classList.remove('hide');
 });
 
 });
