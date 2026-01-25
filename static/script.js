ff2e9e895e15886dae1a4197d6a058a14301e52a
Nasbo <207200135+NasboGetsTheHub@users.noreply.github.com>
2026-01-25 17:48:49 +0200

    Replace 'ocs' button functionality with 'article'

--- static/script.js
+++ static/script.js
@@ -7,8 +7,8 @@ document.getElementById('aboutme-pge').classList.remove('hide');
 document.getElementById('gimmicks-btn').classList.remove('active');
 document.getElementById('gimmicks-pge').classList.add('hide');
 
-document.getElementById('ocs-btn').classList.remove('active');
-document.getElementById('ocs-pge').classList.add('hide');
+document.getElementById('article-btn').classList.remove('active');
+document.getElementById('article-pge').classList.add('hide');
 
 document.getElementById('comforts-btn').classList.remove('active');
 document.getElementById('comforts-pge').classList.add('hide');
@@ -23,8 +23,8 @@ document.getElementById('aboutme-pge').classList.add('hide');
 document.getElementById('gimmicks-btn').classList.add('active');
 document.getElementById('gimmicks-pge').classList.remove('hide');
 
-document.getElementById('ocs-btn').classList.remove('active');
-document.getElementById('ocs-pge').classList.add('hide');
+document.getElementById('article-btn').classList.remove('active');
+document.getElementById('article-pge').classList.add('hide');
 
 document.getElementById('comforts-btn').classList.remove('active');
 document.getElementById('comforts-pge').classList.add('hide');
@@ -32,15 +32,15 @@ document.getElementById('comforts-pge').classList.add('hide');
 
 /* */
 
-document.getElementById('ocs-btn').addEventListener('click', function() {
+document.getElementById('article-btn').addEventListener('click', function() {
 document.getElementById('aboutme-btn').classList.remove('active');
 document.getElementById('aboutme-pge').classList.add('hide');
 
 document.getElementById('gimmicks-btn').classList.remove('active');
 document.getElementById('gimmicks-pge').classList.add('hide');
 
-document.getElementById('ocs-btn').classList.add('active');
-document.getElementById('ocs-pge').classList.remove('hide');
+document.getElementById('article-btn').classList.add('active');
+document.getElementById('article-pge').classList.remove('hide');
 
 document.getElementById('comforts-btn').classList.remove('active');
 document.getElementById('comforts-pge').classList.add('hide');
@@ -55,8 +55,8 @@ document.getElementById('aboutme-pge').classList.add('hide');
 document.getElementById('gimmicks-btn').classList.remove('active');
 document.getElementById('gimmicks-pge').classList.add('hide');
 
-document.getElementById('ocs-btn').classList.remove('active');
-document.getElementById('ocs-pge').classList.add('hide');
+document.getElementById('article-btn').classList.remove('active');
+document.getElementById('article-pge').classList.add('hide');
 
 document.getElementById('comforts-btn').classList.add('active');
 document.getElementById('comforts-pge').classList.remove('hide');
